c7d80c9a3424efed520f48e82d7dfed5ab0d14b0
Italo Amaya <italoamaya@pc-205-236.customer.ask4.lan>
2021-09-19 21:02:23 +0100

    update

---
+++ Emacs/.emacs-profiles.el
@@ -0,0 +1,5 @@
+(("default" . ((user-emacs-directory . "~/Personal/Programing/Repos/github.com/italoaa/Emacs/Configs/Efs/")))
+ ("deamon" . ((user-emacs-directory . "~/Personal/Programing/Emacs/Configs/Efs-Daemon/")))
+ ("doom" . ((user-emacs-directory . "/Users/italoamaya/Personal/Programing/Emacs/Configs/doom-emacs/")))
+ ("space" . ((user-emacs-directory . "/Users/italoamaya/Personal/Programing/Emacs/Configs/Spacemacs/"))))
+
