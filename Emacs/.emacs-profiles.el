d98c08caf454d99ecad0b632a8013bab08ddd9dc
Italo Amaya <italo@pc-203-216.customer.ask4.lan>
2021-11-09 09:25:40 +0000

    This commit is an update using magit and doom emac

--- Emacs/.emacs-profiles.el
+++ Emacs/.emacs-profiles.el
@@ -1,5 +1,4 @@
-(("default" . ((user-emacs-directory . "~/Personal/Programing/Repos/github.com/italoaa/Emacs/Configs/Efs/")))
- ("deamon" . ((user-emacs-directory . "~/Personal/Programing/Emacs/Configs/Efs-Daemon/")))
- ("doom" . ((user-emacs-directory . "/Users/italoamaya/Personal/Programing/Emacs/Configs/doom-emacs/")))
- ("space" . ((user-emacs-directory . "/Users/italoamaya/Personal/Programing/Emacs/Configs/Spacemacs/"))))
+(("default" . ((user-emacs-directory . "~/Personal/Programing/Repos/github.com/italoaa/Emacs/Configs/doom-emacs/")))
+ ("efs" . ((user-emacs-directory . "~/Personal/Programing/Repos/github.com/italoaa/Emacs/Configs/Efs/")))
+ )
 
