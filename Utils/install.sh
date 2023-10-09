b0567a034c2764a7e35ac67990e49ae0079f62f0
Italo Amaya <italo@Italos-MacBook-Pro.local>
2023-10-09 09:13:51 +0100

    Updated my Emacs

--- Utils/install.sh
+++ Utils/install.sh
@@ -7,7 +7,7 @@ xcode-select --install
 export HOMEBREW_BREW_GIT_REMOTE="..."  # put your Git mirror of Homebrew/brew here
 export HOMEBREW_CORE_GIT_REMOTE="..."  # put your Git mirror of Homebrew/homebrew-core here
 
-caffeinate -idm NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
 
 # install via brew
 caffeinate -idm /bin/bash -c "$(brew bundle --file=./Brewfile)"
