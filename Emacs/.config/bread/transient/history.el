0bf494ad6419aaa5ddc3f875cae06b1b42b657ae
Italo Amaya <italo@Italos-MacBook-Pro.local>
2024-01-17 16:42:18 +0000

    Fixed some git issues

--- Emacs/.config/bread/transient/history.el
+++ Emacs/.config/bread/transient/history.el
@@ -1,4 +1,14 @@
 ((magit-am
   ("--3way"))
+ (magit-branch nil)
  (magit-commit nil)
- (magit-push nil))
+ (magit-dispatch nil)
+ (magit-fetch nil)
+ (magit-merge nil)
+ (magit-pull nil)
+ (magit-push nil)
+ (magit-rebase nil)
+ (magit-remote
+  ("-f"))
+ (magit-reset nil)
+ (magit-stash nil))
